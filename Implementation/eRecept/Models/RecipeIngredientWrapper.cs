557a9fa803fbbedc8a31547cb037060e82dab68e
Kanita <kdervic1@etf.unsa.ba>
2020-06-07 11:58:29 +0200

    Added wrapper

---
+++ Implementation/eRecept/Models/RecipeIngredientWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eRecept.Models
+{
+    public class RecipeIngredientWrapper
+    {
+        public RecipeIngredientWrapper(Recipe recipe, Ingredient ingredient)
+        {
+            Recipe = recipe;
+            Ingredient = ingredient;
+        }
+
+        public Recipe Recipe { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
